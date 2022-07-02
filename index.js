f7ca302b2d7063bd9f0de35c34fe8022aa441898
joaofelix27 <joaofelix27@gmail.com>
2022-07-01 21:35:07 -0300

    Fazendo login com dados do front

--- index.js
+++ index.js
@@ -17,32 +17,61 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("banco_de_dados_wallet");
 });
 app.post("/cadastro", async (req, res) => {
-    let { email, name, password } = req.body;
-    // const validation = userSchema.validate(req.body, { abortEarly: true });
-  
-    // if (validation.error) {
-    //   res.sendStatus(422);
-    //   return;
-    // }
-    try {
-      const alreadyExists = await db
-        .collection("users")
-        .find({ name: name })
-        .toArray();
-      console.log(alreadyExists); // if the current name already exists, this variable.lenght is !0
-      if (alreadyExists.length === 0) {
-        await db.collection("users").insertOne({
-          email,
-          name,
-          password
-        });
-        res.sendStatus(201);
-      } else {
-        res.sendStatus(409);
-      }
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
+  let { email, name, password } = req.body;
+  // const validation = userSchema.validate(req.body, { abortEarly: true });
+
+  // if (validation.error) {
+  //   res.sendStatus(422);
+  //   return;
+  // }
+  try {
+    const alreadyExists = await db
+      .collection("users")
+      .find({ email: email })
+      .toArray();
+    console.log(alreadyExists); // if the current name already exists, this variable.lenght is !0
+    if (alreadyExists.length === 0) {
+      await db.collection("users").insertOne({
+        email,
+        name,
+        password,
+      });
+      res.sendStatus(201);
+    } else {
+      res.sendStatus(409);
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  // const validation = userSchema.validate(req.body, { abortEarly: true });
+
+  // if (validation.error) {
+  //   res.sendStatus(422);
+  //   return;
+  // }
+  try {
+    const loginData = await db
+      .collection("users")
+      .find({ email: email })
+      .toArray();
+    if (loginData.length !== 0) {
+      if (email == loginData[0].email && password == loginData[0].password) {
+        res.sendStatus(200);
+        
+      }  else {
+        console.log(loginData[0].email)
+        res.status(401).send("E-mail ou senha inválidos!");
+      }
+    } else {
+      res.status(401).send("E-mail ou senha inválidos!");
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 app.listen(5000);
