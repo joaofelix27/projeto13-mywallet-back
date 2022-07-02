8dc0f9e9b1dd2a7c279a466b61cffcdc628488e0
joaofelix27 <joaofelix27@gmail.com>
2022-07-02 20:45:30 -0300

    Versão com senha encriptada e com token

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import joi from "joi";
+import bcrypt from "bcrypt";
+import {v4 as uuid} from 'uuid'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const userSchema = joi.object({
-  name: joi.string().alphanum().required(),
+  name: joi
+    .string()
+    .pattern(/^[a-zA-Z\s]*$/)
+    .required(), // Only accepts upper or lower case letters or blank spaces
+  email: joi.string().email().required(),
+  password: joi.string().required(),
 });
 
 const mongoClient = new MongoClient("mongodb://localhost:27017");
@@ -18,24 +25,23 @@ mongoClient.connect().then(() => {
 });
 app.post("/cadastro", async (req, res) => {
   let { email, name, password } = req.body;
-  // const validation = userSchema.validate(req.body, { abortEarly: true });
+  const { error } = userSchema.validate(req.body);
 
-  // if (validation.error) {
-  //   res.sendStatus(422);
-  //   return;
-  // }
+  const encryptedPassword = bcrypt.hashSync(password, 10);
+
+  if (error) {
+    res.sendStatus(422);
+    return;
+  }
   try {
     const alreadyExists = await db
       .collection("users")
       .find({ email: email })
       .toArray();
-    console.log(alreadyExists); // if the current name already exists, this variable.lenght is !0
     if (alreadyExists.length === 0) {
-      await db.collection("users").insertOne({
-        email,
-        name,
-        password,
-      });
+      await db
+        .collection("users")
+        .insertOne({ ...req.body, password: encryptedPassword });
       res.sendStatus(201);
     } else {
       res.sendStatus(409);
@@ -47,23 +53,28 @@ app.post("/cadastro", async (req, res) => {
 });
 app.post("/login", async (req, res) => {
   let { email, password } = req.body;
-  // const validation = userSchema.validate(req.body, { abortEarly: true });
+  const userSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+  const { error } = userSchema.validate(req.body);
 
-  // if (validation.error) {
-  //   res.sendStatus(422);
-  //   return;
-  // }
+  if (error) {
+    res.sendStatus(422);
+    return;
+  }
   try {
-    const loginData = await db
-      .collection("users")
-      .find({ email: email })
-      .toArray();
+    const loginData = await db.collection("users").findOne({ email: email });
+    const verifyPassword =bcrypt.compareSync(password,loginData.password)
     if (loginData.length !== 0) {
-      if (email == loginData[0].email && password == loginData[0].password) {
-        res.sendStatus(200);
-        
-      }  else {
-        console.log(loginData[0].email)
+      if (email == loginData.email && verifyPassword) {
+        const token = uuid()
+        await db.collection('sessions').insertOne({
+          token,
+          userId: loginData._id
+        })
+        res.status(200).send({token});
+      } else {
         res.status(401).send("E-mail ou senha inválidos!");
       }
     } else {
@@ -74,4 +85,23 @@ app.post("/login", async (req, res) => {
     res.sendStatus(500);
   }
 });
+
+app.get ("/posts", async (req,res) => {
+const {authorization}= req.headers
+const token = authorization?.replace('Bearer ','')
+
+// Verify if the token is valid
+const session =  await db.collection("sessions").findOne({token})
+
+if (!session) {
+  return res.status(404).send("Token inválido");
+}
+
+const activeUser= await db.collection("users").findOne({_id: new ObjectId(session.userId) })
+console.log(activeUser)
+
+res.send(activeUser)
+
+})
 app.listen(5000);
+ 
