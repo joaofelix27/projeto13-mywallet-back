6cfa6f0b1fde59f49896dbfbf3d70347766a5e24
joaofelix27 <joaofelix27@gmail.com>
2022-07-03 16:34:40 -0300

    Recebendo registros do front

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,5 +1,26 @@
-export  async function postsUser(req, res) {
-    const activeUser = res.locals.activeUser
-  
-    res.send(activeUser);
+import { db, objectId } from "../db/mongo.js";
+
+export async function postsUser(req, res) {
+  const activeUser = res.locals.activeUser;
+
+  res.send(activeUser);
+}
+
+export async function register(req, res) {
+  const activeUser = res.locals.activeUser;
+  let { value, description, type } = req.body;
+  try {
+    await db.collection("register").insertOne({
+      value,
+      description,
+      type,
+      _id:  new objectId(activeUser._id)
+    });
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
   }
+
+  res.send(activeUser);
+}
--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import {postsUser} from '../controllers/postController.js'
+import {postsUser, register} from '../controllers/postController.js'
 import validateUser from "../middlewares/validateUserMiddleware.js";
 
 
 const router = Router ()
 
 router.get("/posts", validateUser, postsUser);
+
+router.post("/entrada", validateUser, register);
 
 export default router;
