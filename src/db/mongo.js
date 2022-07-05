abac907e96b94b95c05808f6ef9e342cc08ffcc4
joaofelix27 <joaofelix27@gmail.com>
2022-07-05 00:11:39 -0300

    Alterando para deploy

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,6 +1,7 @@
 import { MongoClient, ObjectId } from "mongodb";
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+// const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI); 
 let db;
 mongoClient.connect().then(() => {
   db = mongoClient.db("banco_de_dados_wallet");
--- src/index.js
+++ src/index.js
@@ -10,4 +10,7 @@ app.use(express.json(), cors());
 app.use(authRouter);
 app.use(postsRouter);
 
-app.listen(5000);
+// app.listen(5000);
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
