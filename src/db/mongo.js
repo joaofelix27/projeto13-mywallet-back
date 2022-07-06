ec1d012dc916553e7974ea4c50c501167edd8dc4
joaofelix27 <joaofelix27@gmail.com>
2022-07-06 11:31:09 -0300

    Base Repositório

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,11 +1,9 @@
 import { MongoClient, ObjectId } from "mongodb";
 
-// const mongoClient = new MongoClient("mongodb://localhost:27017");
 const mongoClient = new MongoClient(process.env.MONGO_URI); 
 let db;
 mongoClient.connect().then(() => {
-  db = mongoClient.db("banco_de_dados_wallet");
-  // TALVEZ TENHA QUE ALTERAR ISSO process.env.MONGO_DATABASE(cluster0)
+  db = mongoClient.db("banco_de_dados_narutinStore");
 });
 
 const objectId= ObjectId
